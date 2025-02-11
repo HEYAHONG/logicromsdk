3db4e5ce486ec0fdef161e087c7d964ccfee15e8
Ajay Bhargav <ajay.bhargav@waybyte.in>
2025-02-11 14:12:07 +0530

    Fota:add API to request fota aboard
    Signed-off-by: Ajay Bhargav <ajay.bhargav@waybyte.in>

--- include/fota.h
+++ include/fota.h
@@ -28,6 +28,13 @@ int fota_init(void);
 int fota_start(const char *in_url);
 
 /**
+ * @brief Request to abort firmware update
+ * 
+ * @return return 0 on success, negative value on error
+ */
+int fota_abort(void);
+
+/**
  * Get OTA status
  * @return			Return 1 if FOTA in progress, 0 otherwise
  */
